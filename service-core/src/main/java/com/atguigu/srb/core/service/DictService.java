1789628baf00ba61af35bc9194ebb1801b4cad0c
baifengxiao <baifengxiao>
2022-04-04 20:51:58 +0800

    完成上传下载功能

--- service-core/src/main/java/com/atguigu/srb/core/service/DictService.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/DictService.java
@@ -1,7 +1,14 @@
 package com.atguigu.srb.core.service;
 
+import com.alibaba.excel.EasyExcel;
+import com.atguigu.srb.core.listener.ExcelDictDTOListener;
 import com.atguigu.srb.core.pojo.entity.Dict;
+import com.atguigu.srb.core.pojo.entity.dto.ExcelDictDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +20,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface DictService extends IService<Dict> {
 
+    public void importData(InputStream inputStream);
+
+
+    List<ExcelDictDTO> listDictData();
 }
