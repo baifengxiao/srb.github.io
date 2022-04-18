af0ab89c8e29126cd1a62e3894e0515874530e9e
baifengxiao <baifengxiao>
2022-04-19 01:02:55 +0800

    缓存问题，上传文件试试

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/api/BorrowInfoController.java
@@ -0,0 +1,50 @@
+package com.atguigu.srb.core.controller.api;
+
+
+import com.atguigu.common.result.R;
+import com.atguigu.srb.base.util.JwtUtils;
+import com.atguigu.srb.core.service.BorrowInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+
+/**
+ * <p>
+ * 借款信息表 前端控制器
+ * </p>
+ *
+ * @author yupengtao
+ * @since 2022-03-30
+ */
+@Api(tags = "借款信息")
+@RestController
+@RequestMapping("/api/core/borrowInfo")
+@Slf4j
+public class BorrowInfoController {
+    @Resource
+    private BorrowInfoService borrowInfoService;
+
+    @ApiOperation("获取借款额度")
+    @GetMapping("/auth/getBorrowAmount")
+
+    public R getBorrowAmount(HttpServletRequest request) {
+
+        String token = request.getHeader("token");
+
+        Long userId = JwtUtils.getUserId(token);
+
+        BigDecimal borrowAmount = borrowInfoService.getBorrowAmount(userId);
+
+        return R.ok().data("borrowAmount", borrowAmount);
+
+    }
+}
+
--- service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
@@ -1,10 +1,20 @@
 package com.atguigu.srb.core.service.impl;
 
+import com.atguigu.common.exception.Assert;
+import com.atguigu.common.result.ResponseEnum;
+import com.atguigu.srb.core.mapper.IntegralGradeMapper;
+import com.atguigu.srb.core.mapper.UserInfoMapper;
 import com.atguigu.srb.core.pojo.entity.BorrowInfo;
 import com.atguigu.srb.core.mapper.BorrowInfoMapper;
+import com.atguigu.srb.core.pojo.entity.IntegralGrade;
+import com.atguigu.srb.core.pojo.entity.UserInfo;
 import com.atguigu.srb.core.service.BorrowInfoService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
 
 /**
  * <p>
@@ -17,4 +27,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class BorrowInfoServiceImpl extends ServiceImpl<BorrowInfoMapper, BorrowInfo> implements BorrowInfoService {
 
+    @Resource
+    private UserInfoMapper userInfoMapper;
+
+    @Resource
+    private IntegralGradeMapper integralGradeMapper;
+    @Override
+    public BigDecimal getBorrowAmount(Long userId) {
+        //获取用户积分
+        UserInfo userInfo = userInfoMapper.selectById(userId);
+        Assert.notNull(userInfo, ResponseEnum.LOGIN_MOBILE_ERROR);
+        Integer integral = userInfo.getIntegral();
+
+        //根据积分查询额度
+        QueryWrapper<IntegralGrade> integralGradeQueryWrapper = new QueryWrapper<>();
+        integralGradeQueryWrapper
+                .le("integral_start",integral)
+                .ge("integral_end",integral);
+        IntegralGrade integralGrade = integralGradeMapper.selectOne(integralGradeQueryWrapper);
+        if (integralGrade==null){
+            return new BigDecimal("0");
+        }
+        return integralGrade.getBorrowAmount();
+
+    }
 }
