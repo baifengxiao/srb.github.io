54eb409f6dd9474760ba1e2dfcdc1bdd9d73719e
baifengxiao <baifengxiao>
2022-04-19 01:14:56 +0800

    后端完成

--- service-base/src/main/java/com/atguigu/srb/base/config/LocalDateTimeSerializerConfig.java
+++ service-base/src/main/java/com/atguigu/srb/base/config/LocalDateTimeSerializerConfig.java
@@ -1,31 +1,31 @@
-package com.atguigu.srb.base.config;
-
-
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-/**
- * @author: baifengxiao
- * @date: 2022/4/12 16:57
- */
-@Configuration
-public class LocalDateTimeSerializerConfig {
-
-    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
-    private String pattern;
-
-    public LocalDateTimeSerializer localDateTimeDeserializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
-    }
-}
+//package com.atguigu.srb.base.config;
+//
+//
+//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//
+///**
+// * @author: baifengxiao
+// * @date: 2022/4/12 16:57
+// */
+//@Configuration
+//public class LocalDateTimeSerializerConfig {
+//
+//    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+//    private String pattern;
+//
+//    public LocalDateTimeSerializer localDateTimeDeserializer() {
+//        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+//    }
+//
+//    @Bean
+//    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+//        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+//    }
+//}
--- service-core/src/main/java/com/atguigu/srb/core/config/MybatisPlusConfig.java
+++ service-core/src/main/java/com/atguigu/srb/core/config/MybatisPlusConfig.java
@@ -1,26 +1,26 @@
-//package com.atguigu.srb.core.config;
-//
-//import com.baomidou.mybatisplus.annotation.DbType;
-//import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-//import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//
-///**
-// * @author: baifengxiao
-// * @date: 2022/3/30 16:36
-// */
-//@Configuration
-//@MapperScan("com.atguigu.srb.core.mapper")
-//@EnableTransactionManagement //事务处理
-//public class MybatisPlusConfig {
-//    @Bean
-//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-//
-//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
-//        return interceptor;
-//    }
-//}
+package com.atguigu.srb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author: baifengxiao
+ * @date: 2022/3/30 16:36
+ */
+@Configuration
+@MapperScan("com.atguigu.srb.core.mapper")
+@EnableTransactionManagement //事务处理
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
+        return interceptor;
+    }
+}
